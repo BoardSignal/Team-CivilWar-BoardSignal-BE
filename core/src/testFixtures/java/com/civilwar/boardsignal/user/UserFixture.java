7fdc1df12aaba48c1ff0f5253aaabd6df1f92f6c
kkangh00n <kig2454@gmail.com>
2024-02-12 02:27:46 +0900

    fix : UserFixture 불필요 fixture 삭제

--- core/src/testFixtures/java/com/civilwar/boardsignal/user/UserFixture.java
+++ core/src/testFixtures/java/com/civilwar/boardsignal/user/UserFixture.java
@@ -6,37 +6,15 @@ import com.civilwar.boardsignal.boardgame.domain.constant.Category;
 import com.civilwar.boardsignal.user.domain.constants.AgeGroup;
 import com.civilwar.boardsignal.user.domain.constants.Gender;
 import com.civilwar.boardsignal.user.domain.entity.User;
-import com.civilwar.boardsignal.user.domain.entity.UserCategory;
-import com.civilwar.boardsignal.user.dto.request.UserJoinRequest;
 import java.util.List;
-import java.util.stream.Stream;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = PRIVATE)
 public class UserFixture {
 
-    public static UserJoinRequest getUserJoinRequest(String providerId) {
-
-        return new UserJoinRequest(
-            "abc1234@gmail.com",
-            "최인준",
-            "injuning",
-            "kakao",
-            providerId,
-            List.of(Category.FAMILY, Category.PARTY),
-            "2호선",
-            "사당역",
-            2000,
-            "20~29",
-            "male"
-        );
-    }
-
     public static User getUserFixture(String providerId, String imageUrl) {
 
-        List<UserCategory> userCategories = Stream.of(Category.FAMILY, Category.PARTY)
-            .map(UserCategory::of)
-            .toList();
+        List<Category> categories = List.of(Category.FAMILY, Category.PARTY);
 
         return User.of(
             "abc1234@gmail.com",
@@ -44,7 +22,7 @@ public class UserFixture {
             "injuning",
             "kakao",
             providerId,
-            userCategories,
+            categories,
             "2호선",
             "사당역",
             imageUrl,
