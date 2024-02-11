29e7e8a9530ff66cea8063785f966fbed2566676
kkangh00n <kig2454@gmail.com>
2024-02-12 02:18:11 +0900

    refactor : UserService 회원 가입 로직 수정

--- core/src/main/java/com/civilwar/boardsignal/user/application/UserService.java
+++ core/src/main/java/com/civilwar/boardsignal/user/application/UserService.java
@@ -4,16 +4,13 @@ import com.civilwar.boardsignal.image.domain.ImageRepository;
 import com.civilwar.boardsignal.user.domain.constants.AgeGroup;
 import com.civilwar.boardsignal.user.domain.constants.Gender;
 import com.civilwar.boardsignal.user.domain.entity.User;
-import com.civilwar.boardsignal.user.domain.entity.UserCategory;
 import com.civilwar.boardsignal.user.domain.repository.UserRepository;
 import com.civilwar.boardsignal.user.dto.request.UserJoinRequest;
 import com.civilwar.boardsignal.user.dto.response.UserJoinResponse;
-import com.civilwar.boardsignal.user.mapper.UserCoreMapper;
-import java.util.List;
+import com.civilwar.boardsignal.user.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -23,16 +20,10 @@ public class UserService {
     private final ImageRepository imageRepository;
 
     @Transactional
-    public UserJoinResponse joinUser(UserJoinRequest userJoinRequest, MultipartFile image) {
+    public UserJoinResponse joinUser(UserJoinRequest userJoinRequest) {
 
         //profileImage 저장
-        String profileImageUrl = imageRepository.save(image);
-
-        //Category -> UserCategory
-        List<UserCategory> userCategories = userJoinRequest.categories()
-            .stream()
-            .map(UserCategory::of)
-            .toList();
+        String profileImageUrl = imageRepository.save(userJoinRequest.image());
 
         //Dto -> User
         User user = User.of(
@@ -41,7 +32,7 @@ public class UserService {
             userJoinRequest.nickName(),
             userJoinRequest.provider(),
             userJoinRequest.providerId(),
-            userCategories,
+            userJoinRequest.categories(),
             userJoinRequest.line(),
             userJoinRequest.station(),
             profileImageUrl,
@@ -50,11 +41,8 @@ public class UserService {
             Gender.of(userJoinRequest.gender())
         );
 
-        //연관 관계 매핑
-        user.updateCategories(userCategories);
-
-        User savedUser = userRepository.save(user);
-        return UserCoreMapper.of(savedUser);
+        userRepository.save(user);
+        return UserMapper.toUserJoinResponse(user);
     }
 
 }
