9d00f3ab04b0ecec2bf28af67cc846b1922684cd
kkangh00n <kig2454@gmail.com>
2024-02-12 02:36:51 +0900

    docs : UserApiController Api 명세

--- api/src/main/java/com/civilwar/boardsignal/user/presentation/UserApiController.java
+++ api/src/main/java/com/civilwar/boardsignal/user/presentation/UserApiController.java
@@ -5,6 +5,9 @@ import com.civilwar.boardsignal.user.dto.request.ApiUserJoinRequest;
 import com.civilwar.boardsignal.user.dto.request.UserJoinRequest;
 import com.civilwar.boardsignal.user.dto.response.UserJoinResponse;
 import com.civilwar.boardsignal.user.mapper.UserApiMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,21 +17,25 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Tag(name = "User API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
-public class UserController {
+public class UserApiController {
 
     private final UserService userService;
 
+    @Operation(summary = "회원가입 API")
+    @ApiResponse(useReturnTypeSchema = true)
     @PostMapping
     public ResponseEntity<UserJoinResponse> joinUser(
         @Valid @RequestPart(value = "data") ApiUserJoinRequest apiUserJoinRequest,
         @RequestPart(value = "image", required = false) MultipartFile image
     ) {
-        UserJoinRequest userJoinRequest = UserApiMapper.of(apiUserJoinRequest);
+        UserJoinRequest userJoinRequest = UserApiMapper.toUserJoinRequest(apiUserJoinRequest,
+            image);
 
-        UserJoinResponse userJoinResponse = userService.joinUser(userJoinRequest, image);
+        UserJoinResponse userJoinResponse = userService.joinUser(userJoinRequest);
 
         return ResponseEntity.ok(userJoinResponse);
     }
