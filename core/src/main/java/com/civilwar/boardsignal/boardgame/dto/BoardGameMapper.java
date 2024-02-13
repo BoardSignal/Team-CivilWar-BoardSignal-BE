aef061e0fc9b32fd6d75c08c7639b71e6f475bdf
dlswns2480 <dlswns2480@naver.com>
2024-02-13 20:47:15 +0900

    feat : BoardGame Service 계층에 필요한 응답 dto 구현 - Mapper - 커스텀 응답 dto - 커스텀 페이징 dto

---
+++ core/src/main/java/com/civilwar/boardsignal/boardgame/dto/BoardGameMapper.java
@@ -0,0 +1,42 @@
+package com.civilwar.boardsignal.boardgame.dto;
+
+import com.civilwar.boardsignal.boardgame.domain.entity.BoardGame;
+import com.civilwar.boardsignal.boardgame.dto.response.BoardGamePageResponse;
+import com.civilwar.boardsignal.boardgame.dto.response.GetAllBoardGamesResponse;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class BoardGameMapper {
+
+    public static GetAllBoardGamesResponse toGetAllBoardGamesResponse(BoardGame boardGame) {
+        List<String> boardGameCategories = boardGame.getCategories().stream()
+            .map(category -> category.getCategory().getDescription())
+            .toList();
+        String boardGameDifficulty = boardGame.getDifficulty().getDescription();
+
+        return new GetAllBoardGamesResponse(
+            boardGame.getId(),
+            boardGame.getTitle(),
+            boardGameCategories,
+            boardGameDifficulty,
+            boardGame.getMinParticipants(),
+            boardGame.getMaxParticipants(),
+            boardGame.getFromPlayTime(),
+            boardGame.getToPlayTime(),
+            boardGame.getWishCount(),
+            boardGame.getMainImageUrl()
+        );
+    }
+
+    public static <T> BoardGamePageResponse<T> toBoardGamePageRepsonse(Page<T> page) {
+        return new BoardGamePageResponse<>(
+            page.getContent(),
+            page.getSize(),
+            page.getNumber()
+        );
+    }
+
+}
