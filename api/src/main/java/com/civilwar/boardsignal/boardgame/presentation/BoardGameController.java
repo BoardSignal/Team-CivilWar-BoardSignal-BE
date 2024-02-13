c3c1858aa2fc8effd36653e7bad2644934e2e867
dlswns2480 <dlswns2480@naver.com>
2024-02-13 20:47:53 +0900

    feat : BoardGame 전체 조회 api 구현 및 테스트

---
+++ api/src/main/java/com/civilwar/boardsignal/boardgame/presentation/BoardGameController.java
@@ -0,0 +1,35 @@
+package com.civilwar.boardsignal.boardgame.presentation;
+
+import com.civilwar.boardsignal.boardgame.application.BoardGameService;
+import com.civilwar.boardsignal.boardgame.dto.request.BoardGameSearchCondition;
+import com.civilwar.boardsignal.boardgame.dto.response.GetAllBoardGamesResponse;
+import com.civilwar.boardsignal.boardgame.dto.response.BoardGamePageResponse;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Board Game API")
+@RestController
+@RequestMapping("/api/v1/board-games")
+@RequiredArgsConstructor
+public class BoardGameController {
+
+    private final BoardGameService boardGameService;
+    @ApiResponse(useReturnTypeSchema = true)
+    @GetMapping
+    public ResponseEntity<BoardGamePageResponse<GetAllBoardGamesResponse>> getAllBoardGames(
+        @Parameter(hidden = true) BoardGameSearchCondition condition,
+        Pageable pageable
+    ){
+        BoardGamePageResponse<GetAllBoardGamesResponse> boardGames = boardGameService.getAllBoardGames(
+            condition, pageable);
+        return ResponseEntity.ok(boardGames);
+    }
+}
