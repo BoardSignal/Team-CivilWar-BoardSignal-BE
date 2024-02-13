e32cfe83efde0f85608137feba458b814e48e5f9
dlswns2480 <dlswns2480@naver.com>
2024-02-13 20:41:21 +0900

    feat : BoardGameRepository 어댑터 구현 - 조회용, 생성용

---
+++ core/src/main/java/com/civilwar/boardsignal/boardgame/infrastructure/adaptor/BoardGameQueryRepositoryAdaptor.java
@@ -0,0 +1,66 @@
+package com.civilwar.boardsignal.boardgame.infrastructure.adaptor;
+
+import static com.civilwar.boardsignal.boardgame.domain.entity.QBoardGame.boardGame;
+import static org.springframework.util.StringUtils.hasText;
+
+import com.civilwar.boardsignal.boardgame.domain.constant.Difficulty;
+import com.civilwar.boardsignal.boardgame.domain.entity.BoardGame;
+import com.civilwar.boardsignal.boardgame.domain.repository.BoardGameQueryRepository;
+import com.civilwar.boardsignal.boardgame.dto.request.BoardGameSearchCondition;
+import com.civilwar.boardsignal.boardgame.infrastructure.repository.BoardGameJpaRepository;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class BoardGameQueryRepositoryAdaptor implements BoardGameQueryRepository {
+
+    private final BoardGameJpaRepository boardGameJpaRepository;
+    private final JPAQueryFactory jpaQueryFactory;
+
+    private BooleanExpression equalDifficulty(String difficulty) {
+        if (!hasText(difficulty)) {
+            return null;
+        }
+        return boardGame.difficulty.eq(Difficulty.of(difficulty));
+    }
+
+    private BooleanExpression checkRangePlayTime(Integer playTime) {
+        if (playTime == null) {
+            return null;
+        }
+
+        BooleanExpression isLower = boardGame.fromPlayTime.loe(playTime);
+        BooleanExpression isGreater = boardGame.toPlayTime.goe(playTime);
+
+        return isLower.and(isGreater);
+    }
+
+    @Override
+    public Optional<BoardGame> findById(Long id) {
+        return boardGameJpaRepository.findById(id);
+    }
+
+    @Override
+    public Page<BoardGame> findAll(BoardGameSearchCondition condition, Pageable pageable) {
+        List<BoardGame> boardGames = jpaQueryFactory
+            .selectFrom(boardGame)
+            .where(
+                equalDifficulty(condition.difficulty()),
+                checkRangePlayTime(condition.playTime())
+            )
+            .orderBy(boardGame.wishCount.desc())
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .fetch();
+
+        return new PageImpl<>(boardGames);
+    }
+}
