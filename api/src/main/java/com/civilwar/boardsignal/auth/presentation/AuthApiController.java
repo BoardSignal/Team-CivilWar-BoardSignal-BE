3e0d8ff295ff2ea8a171f258d31a54ba4c5568c1
kkangh00n <kig2454@gmail.com>
2024-02-14 20:49:21 +0900

    docs : AuthApiController 명세

--- api/src/main/java/com/civilwar/boardsignal/auth/presentation/AuthApiController.java
+++ api/src/main/java/com/civilwar/boardsignal/auth/presentation/AuthApiController.java
@@ -2,6 +2,8 @@ package com.civilwar.boardsignal.auth.presentation;
 
 import com.civilwar.boardsignal.auth.application.AuthService;
 import com.civilwar.boardsignal.auth.dto.response.IssueTokenResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Auth API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
@@ -20,6 +23,7 @@ public class AuthApiController {
 
     private final AuthService authService;
 
+    @Operation(summary = "카카오 로그인 API", description = "웹 페이지를 통한 로그인")
     @PostMapping("/login/kakao")
     public ResponseEntity<?> kakaoLoginRedirect() {
         HttpHeaders httpHeaders = new HttpHeaders();
@@ -28,6 +32,7 @@ public class AuthApiController {
         return new ResponseEntity<>(httpHeaders, HttpStatus.MOVED_PERMANENTLY);
     }
 
+    @Operation(summary = "네이버 로그인 API", description = "웹 페이지를 통한 로그인")
     @PostMapping("/login/naver")
     public ResponseEntity<?> naverLoginRedirect() {
         HttpHeaders httpHeaders = new HttpHeaders();
@@ -36,6 +41,7 @@ public class AuthApiController {
         return new ResponseEntity<>(httpHeaders, HttpStatus.MOVED_PERMANENTLY);
     }
 
+    @Operation(summary = "AccessToken 재발급 API", description = "Cookie에 RefreshToken Id 필요")
     @GetMapping("/reissue")
     public ResponseEntity<IssueTokenResponse> issueAccessToken(
         @CookieValue(name = "RefreshTokenId") String refreshTokenId
