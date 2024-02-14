1b6f401c4d8fb189f98069638fe988ed5b18415c
kkangh00n <kig2454@gmail.com>
2024-02-14 23:31:11 +0900

    refactor : RefreshTokenRepository 분리

--- core/src/main/java/com/civilwar/boardsignal/auth/infrastructure/RefreshTokenRedisRepository.java
+++ core/src/main/java/com/civilwar/boardsignal/auth/infrastructure/RefreshTokenRedisRepository.java
@@ -1,5 +1,6 @@
 package com.civilwar.boardsignal.auth.infrastructure;
 
+import com.civilwar.boardsignal.auth.domain.RefreshTokenRepository;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class RefreshTokenRepository {
+public class RefreshTokenRedisRepository implements RefreshTokenRepository {
 
     private final StringRedisTemplate redisTemplate;
 
+    @Override
     public void save(String id, String refreshToken, Long refreshExpiryTime) {
         redisTemplate.opsForValue()
             .set(id, refreshToken, refreshExpiryTime, TimeUnit.MINUTES);
     }
 
+    @Override
     public Optional<String> findById(String id) {
         return Optional.ofNullable(redisTemplate.opsForValue()
             .get(id));
