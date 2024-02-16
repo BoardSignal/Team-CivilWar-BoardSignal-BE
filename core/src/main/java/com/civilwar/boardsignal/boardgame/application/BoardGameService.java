3a8aa05503f8bdd3a54c479dd7c88e0e5e5c7278
dlswns2480 <dlswns2480@naver.com>
2024-02-16 19:36:47 +0900

    edit : 반환값 Slice로 변경에 따른 서비스 코드 수정

--- core/src/main/java/com/civilwar/boardsignal/boardgame/application/BoardGameService.java
+++ core/src/main/java/com/civilwar/boardsignal/boardgame/application/BoardGameService.java
@@ -4,11 +4,11 @@ import com.civilwar.boardsignal.boardgame.domain.entity.BoardGame;
 import com.civilwar.boardsignal.boardgame.domain.repository.BoardGameQueryRepository;
 import com.civilwar.boardsignal.boardgame.dto.BoardGameMapper;
 import com.civilwar.boardsignal.boardgame.dto.request.BoardGameSearchCondition;
-import com.civilwar.boardsignal.boardgame.dto.response.GetAllBoardGamesResponse;
 import com.civilwar.boardsignal.boardgame.dto.response.BoardGamePageResponse;
+import com.civilwar.boardsignal.boardgame.dto.response.GetAllBoardGamesResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,9 +17,11 @@ public class BoardGameService {
 
     private final BoardGameQueryRepository boardGameQueryRepository;
 
-    public BoardGamePageResponse<GetAllBoardGamesResponse> getAllBoardGames(BoardGameSearchCondition condition, Pageable pageable){
-        Page<BoardGame> boardGames = boardGameQueryRepository.findAll(condition, pageable);
-        Page<GetAllBoardGamesResponse> findBoardGames = boardGames.map(BoardGameMapper::toGetAllBoardGamesResponse); // 응답 dto 형식으로 변환
+    public BoardGamePageResponse<GetAllBoardGamesResponse> getAllBoardGames(
+        BoardGameSearchCondition condition, Pageable pageable) {
+        Slice<BoardGame> boardGames = boardGameQueryRepository.findAll(condition, pageable);
+        Slice<GetAllBoardGamesResponse> findBoardGames = boardGames.map(
+            BoardGameMapper::toGetAllBoardGamesResponse); // 응답 dto 형식으로 변환
         return BoardGameMapper.toBoardGamePageRepsonse(findBoardGames); // 커스텀 페이징 응답 dto에 담음
     }
 }
