108203d53324ee0ef72c96f1537b90b5e6a71ef3
kkangh00n <kig2454@gmail.com>
2024-02-19 21:32:36 +0900

    feat : UserReviewFacade
    - 유저가 받은 리뷰에 대한 데이터 정리
    
    1. 유저가 받은 리뷰 전체 조회
    2. 리뷰들을 항목 별로 분류
    3. 항목 별로 좋아요 & 싫어요 점수 합산

---
+++ core/src/main/java/com/civilwar/boardsignal/user/facade/UserReviewFacade.java
@@ -0,0 +1,50 @@
+package com.civilwar.boardsignal.user.facade;
+
+import com.civilwar.boardsignal.review.domain.constant.ReviewContent;
+import com.civilwar.boardsignal.review.domain.entity.Review;
+import com.civilwar.boardsignal.review.domain.entity.ReviewEvaluation;
+import com.civilwar.boardsignal.review.domain.repository.ReviewRepository;
+import com.civilwar.boardsignal.user.dto.response.UserReviewResponse;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserReviewFacade {
+
+    private final ReviewRepository reviewRepository;
+
+    public List<UserReviewResponse> getUserReview(Long userId) {
+
+        //1. 유저에 대한 리뷰 조회
+        List<Review> reviews = reviewRepository.findReviewsByRevieweeId(userId);
+
+        //2. 리뷰 평가 모두 한 곳에 저장
+        List<ReviewEvaluation> userEvaluations = new ArrayList<>();
+        for (Review review : reviews) {
+            userEvaluations.addAll(review.getReviewEvaluations());
+        }
+
+        //리뷰 조회 결과를 저장할 리스트
+        List<UserReviewResponse> result = new ArrayList<>();
+
+        //3. 리뷰 항목 만큼 반복문
+        ReviewContent[] allContents = ReviewContent.values();
+        for (ReviewContent content : allContents) {
+
+            //4. 유저에 대한 각 항목의 좋아요 & 싫어요 합계 계산
+            int totalScore = userEvaluations.stream()
+                .filter(evaluation -> evaluation.getContent().equals(content))
+                .mapToInt(ReviewEvaluation::getIsRecommended)
+                .sum();
+
+            //. 정리된 리뷰 & 데이터 저장
+            result.add(new UserReviewResponse(content.getDescription(), totalScore));
+        }
+
+        return result;
+    }
+
+}
---
+++ core/src/test/java/com/civilwar/boardsignal/user/facade/UserReviewFacadeTest.java
@@ -0,0 +1,57 @@
+package com.civilwar.boardsignal.user.facade;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+import com.civilwar.boardsignal.review.ReviewFixture;
+import com.civilwar.boardsignal.review.domain.entity.Review;
+import com.civilwar.boardsignal.review.domain.entity.ReviewEvaluation;
+import com.civilwar.boardsignal.review.domain.repository.ReviewRepository;
+import com.civilwar.boardsignal.user.dto.response.UserReviewResponse;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class UserReviewFacadeTest {
+
+    @Mock
+    private ReviewRepository reviewRepository;
+
+    @InjectMocks
+    private UserReviewFacade userReviewFacade;
+
+    @Test
+    @DisplayName("[유저가 평가 받은 리뷰를 항목 별로 정리한 데이터로 반환한다.]")
+    void getUserReviewTest() {
+        //given
+        Long revieweeId = 100L;
+        Long roomId = 1L;
+        List<ReviewEvaluation> evaluations = ReviewFixture.getEvaluationFixture();
+        List<Review> reviews = new ArrayList<>();
+        for (long i = 0; i < 3; i++) {
+            Review reviewFixture = ReviewFixture.getReviewFixture(i, revieweeId, roomId,
+                evaluations);
+            reviews.add(reviewFixture);
+        }
+        given(reviewRepository.findReviewsByRevieweeId(revieweeId)).willReturn(reviews);
+
+        //when
+        List<UserReviewResponse> userReviews = userReviewFacade.getUserReview(revieweeId);
+
+        UserReviewResponse timeCommitmentReview = userReviews.get(0);
+        UserReviewResponse goodMannerReview = userReviews.get(1);
+        UserReviewResponse fastResponseReview = userReviews.get(2);
+
+        //then
+        assertThat(userReviews).hasSize(3);
+        assertThat(timeCommitmentReview.score()).isEqualTo(3);
+        assertThat(goodMannerReview.score()).isZero();
+        assertThat(fastResponseReview.score()).isEqualTo(-3);
+    }
+}
