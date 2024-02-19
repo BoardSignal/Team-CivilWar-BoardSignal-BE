ce96041db21bb89225b9335d7a7c52d06d9352cb
dlswns2480 <dlswns2480@naver.com>
2024-02-19 11:36:05 +0900

    feat : 찜 관련 repository 정의

---
+++ core/src/main/java/com/civilwar/boardsignal/boardgame/infrastructure/repository/WishJpaRepository.java
@@ -0,0 +1,10 @@
+package com.civilwar.boardsignal.boardgame.infrastructure.repository;
+
+import com.civilwar.boardsignal.boardgame.domain.entity.Wish;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WishJpaRepository extends JpaRepository<Wish, Long> {
+
+    Optional<Wish> findByUserIdAndBoardGameId(Long userId, Long boardGameId);
+}
