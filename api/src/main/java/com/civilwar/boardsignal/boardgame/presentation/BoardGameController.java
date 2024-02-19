00b10b9be21591514a6dedffd1a30a896caf6044
dlswns2480 <dlswns2480@naver.com>
2024-02-19 11:36:58 +0900

    feat : 찜 등록 api 구현 및 테스트

--- api/src/main/java/com/civilwar/boardsignal/boardgame/presentation/BoardGameController.java
+++ api/src/main/java/com/civilwar/boardsignal/boardgame/presentation/BoardGameController.java
@@ -4,13 +4,19 @@ import com.civilwar.boardsignal.boardgame.application.BoardGameService;
 import com.civilwar.boardsignal.boardgame.dto.request.BoardGameSearchCondition;
 import com.civilwar.boardsignal.boardgame.dto.response.BoardGamePageResponse;
 import com.civilwar.boardsignal.boardgame.dto.response.GetAllBoardGamesResponse;
+import com.civilwar.boardsignal.boardgame.dto.response.WishBoardGameResponse;
+import com.civilwar.boardsignal.user.domain.entity.User;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,4 +38,16 @@ public class BoardGameController {
             condition, pageable);
         return ResponseEntity.ok(boardGames);
     }
+
+    @Operation(summary = "보드게임 찜하기 API")
+    @ApiResponse(useReturnTypeSchema = true)
+    @PostMapping("/wish/{boardGameId}")
+    public ResponseEntity<WishBoardGameResponse> wishBoardGame(
+        @AuthenticationPrincipal User user,
+        @PathVariable("boardGameId") Long boardGameId
+    ) {
+        WishBoardGameResponse response = boardGameService.wishBoardGame(user, boardGameId);
+        return ResponseEntity.ok(response);
+    }
+
 }
