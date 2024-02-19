8eaadda6b08b57e98a6a585a0a9cde076077cfc8
dlswns2480 <dlswns2480@naver.com>
2024-02-19 11:35:01 +0900

    feat : 찜 엔티티 생성자 정의

--- core/src/main/java/com/civilwar/boardsignal/boardgame/domain/entity/Wish.java
+++ core/src/main/java/com/civilwar/boardsignal/boardgame/domain/entity/Wish.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -23,4 +25,23 @@ public class Wish {
     private Long userId;
 
     private Long boardGameId;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Wish(
+        Long userId,
+        Long boardGameId
+    ) {
+        this.userId = userId;
+        this.boardGameId = boardGameId;
+    }
+
+    public static Wish of(
+        Long userId,
+        Long boardGameId
+    ) {
+        return Wish.builder()
+            .userId(userId)
+            .boardGameId(boardGameId)
+            .build();
+    }
 }
