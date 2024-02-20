d5b713490c5aaf8e1a9aafaabca7d1c489c28e5b
dlswns2480 <dlswns2480@naver.com>
2024-02-21 01:28:52 +0900

    feat : MultiPartFile 추가에 따른 api 재테스트

--- api/src/test/java/com/civilwar/boardsignal/room/presentation/RoomControllerTest.java
+++ api/src/test/java/com/civilwar/boardsignal/room/presentation/RoomControllerTest.java
@@ -1,7 +1,7 @@
 package com.civilwar.boardsignal.room.presentation;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -9,11 +9,15 @@ import com.civilwar.boardsignal.common.support.ApiTestSupport;
 import com.civilwar.boardsignal.room.domain.entity.Room;
 import com.civilwar.boardsignal.room.domain.repository.RoomRepository;
 import com.civilwar.boardsignal.room.dto.request.ApiCreateRoomRequest;
+import java.io.FileInputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.ResultActions;
 
 @DisplayName("[RoomController 테스트]")
@@ -38,13 +42,30 @@ class RoomControllerTest extends ApiTestSupport {
             "7호선",
             "이수역",
             "레드버튼 사당점",
-            List.of("가족게임", "컬렉터블게임")
+            List.of("가족게임", "컬렉터블게임"),
+            true
         );
 
-        ResultActions resultActions = mockMvc.perform(post("/api/v1/rooms")
-                .contentType(MediaType.APPLICATION_JSON)
-                .header(AUTHORIZATION, accessToken)
-                .content(toJson(request)))
+        String fileName = "testFile.png";
+        MockMultipartFile data = new MockMultipartFile(
+            "data",
+            null,
+            "application/json",
+            toJson(request).getBytes(StandardCharsets.UTF_8)
+        );
+        MockMultipartFile image = new MockMultipartFile(
+            "image",
+            fileName,
+            "image/png",
+            new FileInputStream("src/test/resources/" + fileName)
+        );
+
+        ResultActions resultActions = mockMvc.perform(
+                multipart(HttpMethod.POST, "/api/v1/rooms")
+                    .file(image)
+                    .file(data)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header(AUTHORIZATION, accessToken))
             .andExpect(status().isOk());
 
         Room room = roomRepository.findById(1L).orElseThrow();
