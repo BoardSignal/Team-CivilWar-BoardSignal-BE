a30f4df8e296ac618318d1d437ea39af29a3bf7e
dlswns2480 <dlswns2480@naver.com>
2024-02-20 16:09:12 +0900

    feat : 보드게임 Id를 통해 tip 전체조회 쿼리 추가 및 테스트

--- core/src/main/java/com/civilwar/boardsignal/boardgame/infrastructure/repository/TipJpaRepository.java
+++ core/src/main/java/com/civilwar/boardsignal/boardgame/infrastructure/repository/TipJpaRepository.java
@@ -1,10 +1,13 @@
 package com.civilwar.boardsignal.boardgame.infrastructure.repository;
 
 import com.civilwar.boardsignal.boardgame.domain.entity.Tip;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TipJpaRepository extends JpaRepository<Tip, Long> {
 
     Optional<Tip> findByUserId(Long userId);
+
+    List<Tip> findAllByBoardGameId(Long boardGameId);
 }
