bc39dddeb798cfd78357f0697fe998129f2c6584
dlswns2480 <dlswns2480@naver.com>
2024-02-21 01:29:52 +0900

    feat : 방생성 Service 수정 - MultiPartFile image 저장 - 방 생성자 방장으로 생성 - 이성 허용 여부 생성자에 넣는과정 추가

--- core/src/testFixtures/java/com/civilwar/boardsignal/room/RoomFixture.java
+++ core/src/testFixtures/java/com/civilwar/boardsignal/room/RoomFixture.java
@@ -1,22 +1,27 @@
 package com.civilwar.boardsignal.room;
 
+import com.civilwar.boardsignal.common.MultipartFileFixture;
 import com.civilwar.boardsignal.room.domain.entity.Participant;
 import com.civilwar.boardsignal.room.domain.entity.Room;
 import com.civilwar.boardsignal.room.dto.mapper.RoomMapper;
 import com.civilwar.boardsignal.room.dto.request.CreateRoomResponse;
 import com.civilwar.boardsignal.room.dto.response.CreateRoomRequest;
+import java.io.IOException;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RoomFixture {
 
-    public static Room getRoom() {
-        return RoomMapper.toRoom(getCreateRoomRequest());
+    public static Room getRoom() throws IOException {
+        MockMultipartFile image = MultipartFileFixture.getMultipartFile();
+        return RoomMapper.toRoom("imageUrl", getCreateRoomRequest(image));
     }
 
-    public static CreateRoomRequest getCreateRoomRequest() {
+    public static CreateRoomRequest getCreateRoomRequest(MultipartFile image) {
         return new CreateRoomRequest(
             "무슨무슨 모임입니다!",
             "재밌게 하려고 모집중",
@@ -30,7 +35,10 @@ public class RoomFixture {
             "7호선",
             "이수역",
             "레드버튼 사당점",
-            List.of("가족게임", "컬렉터블게임")
+            List.of("가족게임", "컬렉터블게임"),
+            true,
+            image
+
         );
     }
 
@@ -39,7 +47,7 @@ public class RoomFixture {
     }
 
     public static Participant getParticipant() {
-        return Participant.of(1L, 1L);
+        return Participant.of(1L, 1L, true, true);
     }
 
 }
