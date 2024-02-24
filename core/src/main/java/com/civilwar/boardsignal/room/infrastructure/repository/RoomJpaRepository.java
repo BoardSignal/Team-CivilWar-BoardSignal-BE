024b9c06a50bcd77840c657776ae27e20257ff3e
kkangh00n <kig2454@gmail.com>
2024-02-25 02:05:09 +0900

    feat : Room 단일 조회 fetch join

--- core/src/main/java/com/civilwar/boardsignal/room/infrastructure/repository/RoomJpaRepository.java
+++ core/src/main/java/com/civilwar/boardsignal/room/infrastructure/repository/RoomJpaRepository.java
@@ -2,12 +2,17 @@ package com.civilwar.boardsignal.room.infrastructure.repository;
 
 import com.civilwar.boardsignal.room.domain.entity.Room;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface RoomJpaRepository extends JpaRepository<Room, Long> {
+
+    @EntityGraph(attributePaths = {"roomCategories", "meetingInfo"})
+    @Override
+    Optional<Room> findById(Long roomId);
 
     // 내가 참여한 모든 room 조회 쿼리
     @EntityGraph(attributePaths = {"roomCategories", "meetingInfo"})
