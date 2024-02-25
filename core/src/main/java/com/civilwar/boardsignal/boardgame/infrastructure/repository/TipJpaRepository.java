c886d3e238c9ceec29f9d6cbfe7850893d84e3de
dlswns2480 <dlswns2480@naver.com>
2024-02-25 22:37:07 +0900

    feat : Tip repository 메소드 추가 - 좋아요 로직에 필요한 Lock 적용 메소드 추가

--- core/src/main/java/com/civilwar/boardsignal/boardgame/infrastructure/repository/TipJpaRepository.java
+++ core/src/main/java/com/civilwar/boardsignal/boardgame/infrastructure/repository/TipJpaRepository.java
@@ -1,13 +1,21 @@
 package com.civilwar.boardsignal.boardgame.infrastructure.repository;
 
 import com.civilwar.boardsignal.boardgame.domain.entity.Tip;
+import jakarta.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TipJpaRepository extends JpaRepository<Tip, Long> {
 
     Optional<Tip> findByUserId(Long userId);
 
     List<Tip> findAllByBoardGameId(Long boardGameId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select t from Tip t where t.id = :id")
+    Optional<Tip> findByIdWithLock(@Param("id") Long id);
 }
