119ec34f93e7706fe4678be333da085dd6437f1a
kkangh00n <kig2454@gmail.com>
2024-03-02 23:04:46 +0900

    fix : security cors 설정 삭제

--- api/src/main/java/com/civilwar/boardsignal/common/config/CorsConfig.java
+++ api/src/main/java/com/civilwar/boardsignal/common/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173")
+            .allowedOrigins("*")
             .allowedHeaders(AUTHORIZATION)
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowCredentials(true)
