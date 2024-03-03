c03f5a2159634aca0dbf99e9956c66d5c01e5021
최인준 <54973090+dlswns2480@users.noreply.github.com>
2024-03-03 14:08:47 +0900

    fix : CorsConfig 버그 수정

--- api/src/main/java/com/civilwar/boardsignal/common/config/CorsConfig.java
+++ api/src/main/java/com/civilwar/boardsignal/common/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOriginPatterns("*")
             .allowedHeaders(AUTHORIZATION)
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowCredentials(true)
