b8bccfb67ddbf4154c159b99170af643103684cc
dlswns2480 <dlswns2480@naver.com>
2024-03-06 00:53:59 +0900

    feat : MeetingInfoRepository 정의

---
+++ core/src/main/java/com/civilwar/boardsignal/room/domain/repository/MeetingInfoRepository.java
@@ -0,0 +1,9 @@
+package com.civilwar.boardsignal.room.domain.repository;
+
+import com.civilwar.boardsignal.room.domain.entity.MeetingInfo;
+
+public interface MeetingInfoRepository {
+
+    MeetingInfo save(MeetingInfo meetingInfo);
+
+}
