f9c7f1aa120dd92cd8c9a7aa69f78ae474a619dd
dlswns2480 <dlswns2480@naver.com>
2024-03-06 02:34:55 +0900

    feat : 알림 조회 api 구현 및 테스트

--- api/src/main/java/com/civilwar/boardsignal/notification/presentation/NotificationController.java
+++ api/src/main/java/com/civilwar/boardsignal/notification/presentation/NotificationController.java
@@ -1,17 +1,22 @@
 package com.civilwar.boardsignal.notification.presentation;
 
+import com.civilwar.boardsignal.notification.application.FcmService;
 import com.civilwar.boardsignal.notification.application.NotificationService;
 import com.civilwar.boardsignal.notification.dto.request.CreateFcmTokenReequest;
 import com.civilwar.boardsignal.notification.dto.request.NotificationTestRequest;
 import com.civilwar.boardsignal.notification.dto.response.CreateFcmTokenResponse;
+import com.civilwar.boardsignal.notification.dto.response.NotificationPageResponse;
+import com.civilwar.boardsignal.notification.dto.response.NotificationResponse;
 import com.civilwar.boardsignal.user.domain.entity.User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,12 +28,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class NotificationController {
 
+    private final FcmService fcmService;
     private final NotificationService notificationService;
 
     //test용
     @PostMapping
     public ResponseEntity<String> notificationTest(NotificationTestRequest request) {
-        String response = notificationService.sendMessageTest(request);
+        String response = fcmService.sendMessageTest(request);
         return ResponseEntity.ok(response);
     }
 
@@ -42,5 +48,20 @@ public class NotificationController {
     ) {
         CreateFcmTokenResponse response = notificationService.createFcmToken(user, request);
         return ResponseEntity.ok(response);
+    }
+
+    @Operation(summary = "나의 알림 목록 조회 API")
+    @ApiResponse(useReturnTypeSchema = true)
+    @GetMapping("/my")
+    public ResponseEntity<NotificationPageResponse<NotificationResponse>> getAllNotifications(
+        @Parameter(hidden = true)
+        @AuthenticationPrincipal User user,
+        Pageable pageable
+    ) {
+        NotificationPageResponse<NotificationResponse> resposne = notificationService.getAllNotifications(
+            user, pageable
+        );
+
+        return ResponseEntity.ok(resposne);
     }
 }
