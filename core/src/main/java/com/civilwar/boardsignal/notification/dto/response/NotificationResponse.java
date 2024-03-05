892f3800090607ac922dba03246163afe587e75b
dlswns2480 <dlswns2480@naver.com>
2024-03-06 02:34:12 +0900

    feat : 알림 조회 관련 dto

---
+++ core/src/main/java/com/civilwar/boardsignal/notification/dto/response/NotificationResponse.java
@@ -0,0 +1,10 @@
+package com.civilwar.boardsignal.notification.dto.response;
+
+public record NotificationResponse(
+    Long notificationId,
+    String title,
+    String body,
+    String imageUrl
+) {
+
+}
