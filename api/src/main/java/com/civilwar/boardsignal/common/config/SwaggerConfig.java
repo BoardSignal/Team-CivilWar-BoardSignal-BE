2034efe90087ec46b72a345384a8baeba17d66fd
dlswns2480 <dlswns2480@naver.com>
2024-03-07 12:06:01 +0900

    fix : swagger 접속 오류 수정

--- api/src/main/java/com/civilwar/boardsignal/common/config/SwaggerConfig.java
+++ api/src/main/java/com/civilwar/boardsignal/common/config/SwaggerConfig.java
@@ -1,7 +1,14 @@
 package com.civilwar.boardsignal.common.config;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import java.util.List;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,17 +21,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
-//    @Bean
-//    public OpenAPI openAPI() {
-//        SecurityScheme securityScheme = new SecurityScheme()
-//            .type(SecurityScheme.Type.HTTP)
-//            .in(SecurityScheme.In.HEADER).name(AUTHORIZATION);
-//        SecurityRequirement securityRequirement = new SecurityRequirement().addList(AUTHORIZATION);
-//
-//        return new OpenAPI()
-//            .components(new Components().addSecuritySchemes(AUTHORIZATION, securityScheme))
-//            .security(List.of(securityRequirement));
-//    }
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .in(SecurityScheme.In.HEADER).name(AUTHORIZATION);
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(AUTHORIZATION);
+
+        return new OpenAPI()
+            .components(new Components().addSecuritySchemes(AUTHORIZATION, securityScheme))
+            .security(List.of(securityRequirement));
+    }
 
     @Bean
     public GroupedOpenApi chatOpenApi() {
