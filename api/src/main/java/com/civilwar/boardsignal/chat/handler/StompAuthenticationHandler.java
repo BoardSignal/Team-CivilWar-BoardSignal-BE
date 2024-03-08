8a5695a0a2800c0580b299eb12b06717b425cbc9
kkangh00n <kig2454@gmail.com>
2024-03-08 13:52:06 +0900

    feat : StompAuthenticationHandler -> 웹 소켓 전용 인증 인터셉터

---
+++ api/src/main/java/com/civilwar/boardsignal/chat/handler/StompAuthenticationHandler.java
@@ -0,0 +1,50 @@
+package com.civilwar.boardsignal.chat.handler;
+
+import static org.springframework.http.HttpHeaders.*;
+
+import com.civilwar.boardsignal.auth.exception.AuthErrorCode;
+import com.civilwar.boardsignal.auth.infrastructure.JwtTokenProvider;
+import com.civilwar.boardsignal.common.exception.ValidationException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+@RequiredArgsConstructor
+@Component
+public class StompAuthenticationHandler implements ChannelInterceptor {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private void validateHeader(String token) {
+        if (StringUtils.hasText(token)) {
+            String accessToken = token.split(" ")[1];
+            jwtTokenProvider.validateToken(accessToken);
+        }
+        else{
+            throw new ValidationException(AuthErrorCode.AUTH_REQUIRED);
+        }
+    }
+
+    //클라이언트에서 요청을 보내기 전 실행되는 메서드
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        //Stomp 헤더 접근
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        StompCommand command = accessor.getCommand();
+
+        //첫 연결이거나 메시지를 보낼 때 검증
+        if(command == StompCommand.CONNECT || command == StompCommand.SEND) {
+            //AccessToken 검증
+            String token = accessor.getFirstNativeHeader(AUTHORIZATION);
+            validateHeader(token);
+        }
+
+        return message;
+    }
+
+}
