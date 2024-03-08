5854855040378ed7db6149702482380c7164f6ee
kkangh00n <kig2454@gmail.com>
2024-03-08 13:15:39 +0900

    feat : WebSocketConfig
    1. WebSocket 연결 엔드포인트 설정
    2. 송수신 prefix 설정
    3. WebSocket 내, 인증 핸들러 등록
    4. WebSocket 내에서 발생하는 예외 핸들러 등록

---
+++ api/src/main/java/com/civilwar/boardsignal/common/config/WebSocketConfig.java
@@ -0,0 +1,47 @@
+package com.civilwar.boardsignal.common.config;
+
+import com.civilwar.boardsignal.chat.handler.StompAuthenticationHandler;
+import com.civilwar.boardsignal.chat.handler.StompExceptionHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    // 클라이언트 인증 핸들러
+    private final StompAuthenticationHandler stompAuthenticationHandler;
+    // WebSocket 내 예외 핸들러
+    private final StompExceptionHandler stompExceptionHandler;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //websocket 연결 엔드포인트 설정
+        registry
+            .addEndpoint("/ws/chats")
+            .setAllowedOriginPatterns("*")
+            .withSockJS();
+        //예외 핸들러 등록
+        registry.setErrorHandler(stompExceptionHandler);
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //클라이언트 메시지 수신 구독 설정 경로
+        registry.enableSimpleBroker("/topic");
+        //클라이언트 메시지 송신 경로 prefix
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    //클라이언트 -> 서버 요청 시, 인증 핸들러 등록
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompAuthenticationHandler);
+    }
+}
