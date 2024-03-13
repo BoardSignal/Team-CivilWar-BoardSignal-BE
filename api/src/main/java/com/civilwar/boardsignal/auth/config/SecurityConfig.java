8fede7d1b8ca05ef162012f2200e7b92506c6e07
kkangh00n <kig2454@gmail.com>
2024-03-13 11:18:39 +0900

    edit : 불필요 코드 제거

--- api/src/main/java/com/civilwar/boardsignal/auth/config/SecurityConfig.java
+++ api/src/main/java/com/civilwar/boardsignal/auth/config/SecurityConfig.java
@@ -15,9 +15,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -31,8 +29,6 @@ public class SecurityConfig {
     private final AuthenticationSuccessHandler authenticationSuccessHandler;
     private final JwtExceptionHandlerFilter jwtExceptionHandlerFilter;
     private final UserRepository userRepository;
-//    private final AuthenticationEntryPoint authenticationEntryPoint;
-//    private final AccessDeniedHandler accessDeniedHandler;
 
     private final String[] NEED_AUTHENTICATION = {
         //알림
@@ -66,26 +62,6 @@ public class SecurityConfig {
                     .requestMatchers(HttpMethod.DELETE, "/api/v1/rooms/**").authenticated()
                     .requestMatchers(NEED_AUTHENTICATION).authenticated()
                     .anyRequest().permitAll()
-//                //알림
-//                .requestMatchers("/api/v1/notifications").permitAll()
-//                .requestMatchers(
-//                    new AntPathRequestMatcher("/api/v1/rooms/my/end-games")).hasRole("USER")
-//                //웹소켓
-//                .requestMatchers(new AntPathRequestMatcher("/ws/chats"),
-//                    new AntPathRequestMatcher("/ws/chats/**")).permitAll()
-//                .requestMatchers(HttpMethod.GET,
-//                    //방
-//                    "/api/v1/rooms/**",
-//                    //보드게임
-//                    "/api/v1/board-games/**",
-//                    //인증
-//                    "/api/v1/auth/reissue", "/oauth2/authorization/**",
-//                    //스웨거
-//                    "/swagger-ui/**",
-//                    "/v3/api-docs/**",
-//                    "/"
-//                ).permitAll()
-//                .anyRequest().authenticated()
             )
             //인증 안 된 사용자 접근 시 예외 처리
             .exceptionHandling(configurer -> configurer
@@ -93,7 +69,6 @@ public class SecurityConfig {
                     (request, response, exception)
                         -> {throw new ValidationException(AuthErrorCode.AUTH_REQUIRED);}
                 )
-//                .accessDeniedHandler(accessDeniedHandler)
             )
             //Jwt 관련 예외 처리
             .addFilterBefore(
