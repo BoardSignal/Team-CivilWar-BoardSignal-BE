5079502181a4802fb5cef8e60b8fe439b765a6ca
dlswns2480 <dlswns2480@naver.com>
2024-03-13 20:14:56 +0900

    feat : 토큰으로 Fcm토큰 조회 쿼리 추가

--- core/src/main/java/com/civilwar/boardsignal/user/domain/repository/UserFcmTokenRepository.java
+++ core/src/main/java/com/civilwar/boardsignal/user/domain/repository/UserFcmTokenRepository.java
@@ -1,9 +1,12 @@
 package com.civilwar.boardsignal.user.domain.repository;
 
 import com.civilwar.boardsignal.user.domain.entity.UserFcmToken;
+import java.util.Optional;
 
 public interface UserFcmTokenRepository {
 
     UserFcmToken save(UserFcmToken userFcmToken);
+
+    Optional<UserFcmToken> findByToken(String token);
 
 }
