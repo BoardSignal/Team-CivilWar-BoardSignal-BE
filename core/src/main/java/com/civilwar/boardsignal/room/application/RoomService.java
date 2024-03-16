b86a7c52a55739c269f7fe532daecf20f2323777
kkangh00n <kig2454@gmail.com>
2024-03-17 01:58:07 +0900

    feat : 채팅 페이지를 나간 시점에 대한 필드 추가
    - 방 참여 시, 참여 시간 저장

--- core/src/main/java/com/civilwar/boardsignal/room/application/RoomService.java
+++ core/src/main/java/com/civilwar/boardsignal/room/application/RoomService.java
@@ -81,6 +81,7 @@ public class RoomService {
             savedRoom.getId(),
             true // 방 생성자가 방장여부는 true
         ); // 나중에 방 폭파 때 해당 방의 Participant 전부 삭제
+        participant.updateLastExit(now.get());
         participantRepository.save(participant);
 
         return RoomMapper.toCreateRoomResponse(savedRoom);
