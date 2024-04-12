e356df2535ad9f04697833d5e4e2590d31e4ed89
kkangh00n <kig2454@gmail.com>
2024-04-12 11:24:45 +0900

    refactor : RoomRepository -> 쿼리 수정

--- core/src/main/java/com/civilwar/boardsignal/room/infrastructure/repository/RoomJpaRepository.java
+++ core/src/main/java/com/civilwar/boardsignal/room/infrastructure/repository/RoomJpaRepository.java
@@ -2,8 +2,11 @@ package com.civilwar.boardsignal.room.infrastructure.repository;
 
 import com.civilwar.boardsignal.room.domain.entity.Room;
 import jakarta.persistence.LockModeType;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
@@ -16,15 +19,18 @@ public interface RoomJpaRepository extends JpaRepository<Room, Long> {
     @Override
     Optional<Room> findById(Long roomId);
 
-    // 내가 현재 참여한 room 조회
-    @EntityGraph(attributePaths = {"meetingInfo"})
+    // 내가 현재 참여한 채팅방 조회
+    // 미확정 모임 또는 모임 시간이 (오늘~미래)인 모임
     @Query("select r "
         + "from Room as r "
+        + "left join MeetingInfo as m "
+        + "on m.id = r.meetingInfo.id "
         + "join Participant as p "
         + "on r.id = p.roomId "
         + "where p.userId=:userId "
+        + "and (m.meetingTime>=:today or m.meetingTime is null) "
         + "order by p.createdAt desc")
-    List<Room> findMyGame(@Param("userId") Long userId);
+    Slice<Room> findMyChattingRoom(@Param("userId") Long userId, @Param("today") LocalDateTime today, Pageable pageable);
 
     // 내가 참여한 fix room 조회 쿼리
     @Query("select r "
