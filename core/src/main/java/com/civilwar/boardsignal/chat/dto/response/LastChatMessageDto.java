d9ad76243d75a76b553237de1942a4a6cd21cfba
kkangh00n <kig2454@gmail.com>
2024-05-16 17:44:10 +0900

    fix : 채팅방 조회 시, 마지막 채팅 조회 데이터 필드 추가 (type & createdAt)

--- core/src/main/java/com/civilwar/boardsignal/chat/dto/response/LastChatMessageDto.java
+++ core/src/main/java/com/civilwar/boardsignal/chat/dto/response/LastChatMessageDto.java
@@ -1,8 +1,13 @@
 package com.civilwar.boardsignal.chat.dto.response;
 
+import com.civilwar.boardsignal.chat.domain.constant.MessageType;
+import java.time.LocalDateTime;
+
 public record LastChatMessageDto(
     Long roomId,
-    String content
+    String content,
+    MessageType messageType,
+    LocalDateTime createdAt
 ) {
 
 }
